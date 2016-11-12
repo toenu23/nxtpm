33d82ca3603c80fea40c58157535d90226f0333e
toenu23 <fubarphukhew@gmail.com>
2016-11-12 21:14:21 +0100

    fix tests

--- test/nxt.spec.js
+++ test/nxt.spec.js
@@ -4,9 +4,12 @@ const nxt = new Nxt();
 
 describe('Nxt', function() {
   it('does an API request', function(done) {
-    nxt.request({ requestType: 'getConstants' })
+    nxt.request({ requestType: 'getAlias', aliasName: 'test5' })
     .then(function(data) {
       assert(true);
+      done();
+    }, function(err) {
+      assert(false)
       done();
     });
   });
