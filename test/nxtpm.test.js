b1616739d187aba010924a6e2b74295b020222a4
toenu23 <fubarphukhew@gmail.com>
2016-11-20 12:02:53 +0100

    More unit tests

--- test/nxtpm.test.js
+++ test/nxtpm.test.js
@@ -1,4 +1,7 @@
 const { assert, expect } = require('chai');
+const path = require('path');
+const tmp = require('tmp');
+const jetpack = require('fs-jetpack');
 const nxtpm = require('../index.js');
 const config = require('../lib/util/config.js');
 const nxt = require('../lib/util/nxt.js');
@@ -27,7 +30,8 @@ describe('Validation', () => {
 
 
 describe('Nxt', () => {
-  it('does an API request', done => {
+
+  it('Does an API request', done => {
     nxt.request({ requestType: 'getAlias', aliasName: 'test' })
     .then(data => {
       assert.equal(data.data.aliasName, 'test');
@@ -36,9 +40,12 @@ describe('Nxt', () => {
       console.log(err.stack || err);
     });
   });
+
 });
 
+
 describe('Config', () => {
+
   it('Can set custom config', done => {
     nxtpm.setConfig('nxt:numSources', 1);
     const numSources = config.get('nxt:numSources');
@@ -52,4 +59,93 @@ describe('Config', () => {
       console.log(err.stack || err);
     });
   });
+
+});
+
+
+describe('Info', () => {
+
+  it('Retrieve package info', done => {
+    nxtpm.info('test6').then(data => {
+      expect(data.manifest).not.empty;
+      expect(data.transaction).not.empty;
+      done();
+    }, err => {
+      console.log(err.stack || err);
+    })
+  });
+
+});
+
+
+describe('Install', () => {
+
+  it('Installs package to current working dir', done => {
+    nxtpm.install('test6').then(() => {
+      assert.equal(jetpack.exists('test6'), 'dir');
+      jetpack.remove('test6');
+      done();
+    }, err => {
+      console.log(err.stack || err);
+    })
+  });
+
+  it('Installs package to specified dir', done => {
+    const dir = tmp.dirSync().name;
+    nxtpm.install('test6', dir).then(() => {
+      assert.equal(
+        jetpack.exists(path.join(dir, 'test6')),
+        'dir'
+      );
+      done();
+    });
+  });
+
+  it('Refuses to install to non-empty directory', done => {
+    const dir = tmp.dirSync().name;
+    const file = path.join(dir, 'test6', 'file.txt');
+    jetpack.file(file);
+    expect(() => nxtpm.install('test6', dir)).to.throw(Error);
+    done();
+  });
+
+});
+
+
+describe('Archive', () => {
+
+  it('Create tar.gz archive', done => {
+    const file = tmp.fileSync().name;
+    nxtpm.archive.create(
+      path.join(__dirname, 'package'),
+      file
+    )
+    .then(checksum => {
+      assert.equal(checksum.length, 64);
+      done();
+    }, err => {
+      console.log(err.stack || err);
+    });
+  });
+
+});
+
+
+describe('Publish', () => {
+
+  it('Creates publish transactions', () => {
+    const file = path.join(__dirname, 'package', 'nxtpm.json');
+    const mft = jetpack.read(file, 'json');
+    expect(() => nxtpm.publish(mft, 'test', true)).to.not.throw;
+  });
+
+});
+
+
+describe('Extend', () => {
+
+  it('Creates extend transaction', () => {
+    expect(() => nxtpm.extend('test6', 'test', true)).to.not.throw;
+  });
+
 });
